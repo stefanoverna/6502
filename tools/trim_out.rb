3f021c634d4f595baff867497aaa7b5f2de904e0
Stefano Verna <s.verna@datocms.com>
2021-03-09 22:23:42 +0100

    style

--- tools/trim_out.rb
+++ tools/trim_out.rb
@@ -2,7 +2,7 @@ consecutive_zeros = 0
 
 File.open('b.out', 'wb') do |out|
   File.open('a.out', 'rb') do |file|
-    while char = file.read(1)
+    while (char = file.read(1))
       byte = char.unpack1('C')
 
       consecutive_zeros += 1 if byte.zero?
